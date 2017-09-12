3de766a0a297285a9fd3ed5ff163755157282ec0
Sebastian Ramadan <plebbyastian@gmail.com>
2017-09-12 19:29:55 +1000

    Update common.c

--- server/common.c
+++ server/common.c
@@ -1,5 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <limits.h>
 
 void dump(char const *prefix, void const *data, size_t size, FILE *file)
 {
@@ -34,4 +35,68 @@ void dump(char const *prefix, void const *data, size_t size, FILE *file)
     }
 
     fputc('\n', file);
+}
+
+void pack_little_754(void *destination, long double source, size_t exponent_size, size_t significand_size) {
+    unsigned char *d = destination;
+    int sign = source < 0.0;
+    unsigned long long x = pack_abs_754(sign ? -source : source, exponent_size, significand_size);
+    x = (x >> 7 << 8) + (sign << 7) + (x % 0x80);
+
+    for (size_t size = (exponent_size + significand_size) / 8; size > 0; size--, *d++ = x % 0x100, x /= 0x100);
+}
+
+unsigned long long pack_abs_754(long double source, size_t exponent_size, size_t significand_size) {
+    unsigned long long x = 0;
+
+    while (source >= 2.0) {
+        source /= 2.0;
+        x++;
+    }
+
+    while (source < 1.0) {
+        source *= 2.0;
+        x--;
+    }
+
+    source -= 1.0;
+    source *= (1ULL << significand_size) + 0.5f;
+
+    x += ~(ULLONG_MAX << exponent_size - 1);
+    x <<= significand_size;
+    x |= (unsigned long long) source;
+
+    return x;
+}
+
+long double unpack_little_754(void *source, size_t exponent_size, size_t significand_size) {
+    unsigned char *s = source;
+    unsigned long long x = 0;
+    for (size_t size = (exponent_size + significand_size) / 8; size > 0; x *= 0x100, x += s[--size]);
+
+    int sign = x & 0x80;
+    x = (x >> 8) + x % 0x80;
+
+    return sign ? -unpack_abs_754(x, exponent_size, significand_size) : unpack_abs_754(x, exponent_size, significand_size);
+}
+
+long double unpack_abs_754(unsigned long long source, size_t exponent_size, size_t significand_size) {
+    long double x = source & ~(ULLONG_MAX << significand_size);
+    x /= 1ULL << significand_size;
+    x += 1.0f;
+
+    source >>= significand_size;
+    source -= ~(ULLONG_MAX << exponent_size - 1);
+
+    while (source >= (unsigned long long) LLONG_MIN) {
+        x /= 2.0;
+        source++;
+    }
+
+    while (source > 0) {
+        x *= 2.0;
+        source++;
+    }
+
+    return x;
 }
